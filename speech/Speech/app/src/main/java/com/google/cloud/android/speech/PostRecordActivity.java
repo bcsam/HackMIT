89bafa75d09d649b02cd9982b4565980cb1d396b
bcsam <bcsam@mit.edu>
2017-09-16 18:44:25 -0400

    for pull

--- speech/Speech/app/src/main/java/com/google/cloud/android/speech/PostRecordActivity.java
+++ speech/Speech/app/src/main/java/com/google/cloud/android/speech/PostRecordActivity.java
@@ -1,8 +1,14 @@
 package com.google.cloud.android.speech;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by brentsamuels on 9/16/17.
@@ -21,6 +27,25 @@ public class PostRecordActivity extends AppCompatActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
 
-        ViewPagerAdapter adapter = new ViewPagerAdapter()
+        ViewPagerAdapter adapter = new ViewPagerAdapter();
+    }
+
+    class ViewPagerAdapter extends FragmentStatePagerAdapter{
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
     }
 }
