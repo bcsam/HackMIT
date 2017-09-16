73a10de4ee33f13f20b17e82757055586ba93187
bcsam <bcsam@mit.edu>
2017-09-16 18:41:16 -0400

    for client

---
+++ speech/Speech/app/src/main/java/com/google/cloud/android/speech/PostRecordActivity.java
@@ -0,0 +1,26 @@
+package com.google.cloud.android.speech;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by brentsamuels on 9/16/17.
+ */
+
+public class PostRecordActivity extends AppCompatActivity {
+
+    String transcript;
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_record);
+        transcript = getIntent().getStringExtra("transcript");
+
+        //AnalyzerClient client = new AnalyzerClient();
+        client.getScores(transcript);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter()
+    }
+}
