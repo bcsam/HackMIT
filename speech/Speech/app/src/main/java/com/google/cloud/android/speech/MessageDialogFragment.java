f1a4f15c1783878c6b6975ca84d8003f8dd50d9c
Gabriel Saruhashi <gabrielsaruhashi@Gabriels-MBP.wireless.yale.internal>
2017-09-16 14:25:07 -0400

    HI

--- speech/Speech/app/src/main/java/com/google/cloud/android/speech/MessageDialogFragment.java
+++ speech/Speech/app/src/main/java/com/google/cloud/android/speech/MessageDialogFragment.java
@@ -38,6 +38,7 @@ public class MessageDialogFragment extends AppCompatDialogFragment {
          */
         void onMessageDialogDismissed();
     }
+    // hey
 
     private static final String ARG_MESSAGE = "message";
 
